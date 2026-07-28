db78d953e470187f894eadab2d358d5e1397987d
Barth Sercena <sercenabarth@gmail.com>
2026-07-29 01:10:06 +0800

    added function lesson with example code

---
+++ 13_functions/script.js
@@ -0,0 +1,10 @@
+//function = a section of reusable code. declare code once, use it whenever you want. call the function to execute that code.
+
+function add(x,y){
+    let result = x + y;
+    return result;
+}
+
+let answer = add(2,3)
+console.log(answer);
+
